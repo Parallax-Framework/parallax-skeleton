d8b33920f6d8bc13f3ac5f6c94ba4dd34081d2b7
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-06-22 14:57:06 +0300

    Refactor faction definitions to use local FACTION variable
    Replaced global faction variable initialization with a local FACTION variable in both sh_citizen.lua and sh_police.lua for consistency and improved code clarity. The final registered faction is still assigned to the original global variable.

--- schema/factions/sh_citizen.lua
+++ schema/factions/sh_citizen.lua
@@ -1,9 +1,9 @@
-FACTION_CITIZEN = Parallax.Faction:Instance()
-FACTION_CITIZEN:SetName("Citizen")
-FACTION_CITIZEN:SetDescription("An ordinary citizen living life while wearing ridiculous clothing.")
-FACTION_CITIZEN:SetColor(Color(20, 150, 20))
-FACTION_CITIZEN:MakeDefault()
+local FACTION = Parallax.Faction:Instance()
+FACTION:SetName("Citizen")
+FACTION:SetDescription("An ordinary citizen living life while wearing ridiculous clothing.")
+FACTION:SetColor(Color(20, 150, 20))
+FACTION:MakeDefault()
 
-FACTION_CITIZEN.Image = Parallax.Util:GetMaterial("gamepadui/hl2/chapter1")
+FACTION.Image = Parallax.Util:GetMaterial("gamepadui/hl2/chapter1")
 
-FACTION_CITIZEN = FACTION_CITIZEN:Register()
+FACTION_CITIZEN = FACTION:Register()
--- schema/factions/sh_police.lua
+++ schema/factions/sh_police.lua
@@ -1,16 +1,16 @@
-FACTION_POLICE = Parallax.Faction:Instance()
-FACTION_POLICE:SetName("Police")
-FACTION_POLICE:SetDescription("The police are the law enforcement of the city. They are responsible for maintaining order and enforcing the law.")
-FACTION_POLICE:SetColor(Color(20, 50, 150))
-FACTION_POLICE:MakeDefault()
+local FACTION = Parallax.Faction:Instance()
+FACTION:SetName("Police")
+FACTION:SetDescription("The police are the law enforcement of the city. They are responsible for maintaining order and enforcing the law.")
+FACTION:SetColor(Color(20, 50, 150))
+FACTION:MakeDefault()
 
-FACTION_POLICE:SetModels({
+FACTION:SetModels({
     "models/player/gasmask.mdl",
     "models/player/riot.mdl",
     "models/player/swat.mdl",
     "models/player/urban.mdl"
 })
 
-FACTION_POLICE.Image = Parallax.Util:GetMaterial("gamepadui/hl2/chapter3")
+FACTION.Image = Parallax.Util:GetMaterial("gamepadui/hl2/chapter3")
 
-FACTION_POLICE = FACTION_POLICE:Register()
+FACTION_POLICE = FACTION:Register()
