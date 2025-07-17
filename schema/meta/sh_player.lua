71731f4a20fc800eba4c0a49f587cd614f782fdc
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-17 21:51:20 +0300

    Update sh_player.lua

--- schema/meta/sh_player.lua
+++ schema/meta/sh_player.lua
@@ -3,3 +3,7 @@ local PLAYER = FindMetaTable("Player")
 function PLAYER:IsCitizen()
     return self:Team() == FACTION_CITIZEN
 end
+
+function PLAYER:IsPolice()
+    return self:Team() == FACTION_POLICE
+end
