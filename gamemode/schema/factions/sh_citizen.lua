8d42f21bffc025244c5efb0bb3dbe92d6e2487b3
riggs9162 <riggs9162@gmx.de>
2025-08-10 02:02:16 +0200

    Add Citizen and Police faction definitions
    Introduces new faction files for Citizen and Police, specifying their names, descriptions, colors, images, and available models. These files establish the basic properties and identifiers for each faction in the schema.

--- gamemode/schema/factions/sh_citizen.lua
+++ gamemode/schema/factions/sh_citizen.lua
@@ -1,6 +1,12 @@
 FACTION.Name = "Citizen"
-FACTION.Description = "A member of the citizen faction."
-FACTION.Color = Color(0, 70, 50)
+FACTION.Description = "An ordinary citizen living life while wearing ridiculous clothing.."
+FACTION.Color = Color(20, 150, 20)
 FACTION.IsDefault = true
+
+FACTION.Image = ax.util:GetMaterial("gamepadui/hl2/chapter1")
+
+FACTION.Models = {
+    "models/humans/group01/male_09.mdl"
+}
 
 FACTION_CITIZEN = FACTION.index
---
+++ gamemode/schema/factions/sh_police.lua
@@ -0,0 +1,14 @@
+FACTION.Name = "Police"
+FACTION.Description = "The police are the law enforcement of the city. They are responsible for maintaining order and enforcing the law."
+FACTION.Color = Color(20, 50, 150)
+
+FACTION.Image = ax.util:GetMaterial("gamepadui/hl2/chapter3")
+
+FACTION.Models = {
+    "models/player/gasmask.mdl",
+    "models/player/riot.mdl",
+    "models/player/swat.mdl",
+    "models/player/urban.mdl"
+}
+
+FACTION_POLICE = FACTION.index
